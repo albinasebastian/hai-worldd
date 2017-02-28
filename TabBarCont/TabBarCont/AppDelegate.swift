7298b5fb59f51c60637d423cb5a7b4e0fb405bf5
Albina <albina@macminis-mac-mini.local>
2017-02-28 13:11:09 +0530

    changed in app delegate

--- TabBarCont/TabBarCont/AppDelegate.swift
+++ TabBarCont/TabBarCont/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     //Committted by ram
+    
+    
+    //committed by albina
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
